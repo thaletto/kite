ca05e276ffb03f57f47352b1d0872897cd902d78
Laxman K R <krlaxman03@gmail.com>
2026-03-23 01:47:39 +0530

    update: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className="bg-background border-t border-border px-4 py-3 sm:px-6 text-primary">
+    <footer className="bg-background border-t border-border px-4 py-4 text-primary">
       <p className="m-0 text-sm text-center">
         &copy; {year} Laxman K R. All rights reserved.
       </p>
