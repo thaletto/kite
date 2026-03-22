a5bf794f5ba05cdb374f521df64fcac6e86ee441
Laxman K R <krlaxman03@gmail.com>
2026-03-23 00:09:08 +0530

    feat: added 10 components

--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -1,10 +1,19 @@
 import { CopilotKit } from "@copilotkit/react-core";
 import { CopilotChat } from "@copilotkit/react-ui";
 import { createFileRoute } from "@tanstack/react-router";
-import { useComponent } from "@copilotkit/react-core/v2";
 import "@copilotkit/react-ui/styles.css";
-import { Button } from "#/components/ui/button";
-import { z } from "zod";
+import {
+  useButton,
+  useCard,
+  useInput,
+  useCheckbox,
+  useSelect,
+  useTable,
+  useTabs,
+  useSwitch,
+  useSeparator,
+  useRow
+} from "#/components/chat/useComponent";
 
 export const Route = createFileRoute("/chat")({
   component: App,
@@ -19,13 +28,17 @@ function App() {
 }
 
 function Chat() {
-  useComponent({
-    name: "button",
-    description: "clickable button",
-    parameters: z.object({ text: z.string() }),
-    render: ({ text }: { text: string }) => <Button>{text}</Button>,
-  });
-
+  useButton();
+  useCard();
+  useInput();
+  useCheckbox();
+  useSelect();
+  useTable();
+  useTabs();
+  useSwitch();
+  useSeparator();
+  useRow();
+  
   return (
     <main className="relative z-10 bg-background h-full w-full flex-col overflow-hidden">
       <div className="mx-auto flex min-h-0 h-full w-full max-w-4xl flex-col px-4">
