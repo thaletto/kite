4d59d0c20620c852065a6bf32e8a95016515e8b8
Laxman K R <krlaxman03@gmail.com>
2026-03-23 02:06:18 +0530

    feat: nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
   resolve: {
     tsconfigPaths: true,
   },
