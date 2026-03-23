8a1b904968606d1a67dfa517c8ab77eea1e2cc51
Laxman K R <krlaxman03@gmail.com>
2026-03-23 21:51:19 +0530

    inti: cloudflare wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 const config = defineConfig({
   plugins: [
     devtools(),
     tailwindcss(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
     viteReact(),
   ],
