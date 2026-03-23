1d25e39d9daafa263fe06c82ade3752a1beb7812
Laxman K R <krlaxman03@gmail.com>
2026-03-23 22:25:10 +0530

    deploy: trying netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,10 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 const config = defineConfig({
-  plugins: [
-    devtools(),
-    tailwindcss(),
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
-    tanstackStart(),
-    viteReact(),
-  ],
+  plugins: [devtools(), tailwindcss(), netlify(), tanstackStart(), viteReact()],
   resolve: {
     tsconfigPaths: true,
   },
